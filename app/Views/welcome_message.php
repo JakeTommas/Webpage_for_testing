d096f871da101c78f43013efa465eaa03a31379e
JakeTommas <izaaksadler034@gmail.com>
2020-04-21 19:30:43 +0100

    New commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Dayz Server Start Stop</title>
+	<meta name="description" content="Dayz Server Start stop page">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+</head>
+<body>
+
+<!-- HEADER: MENU + HEROE SECTION -->
+<header>
+	<p>Welcome to the most basic login page ever<p>
+</header>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+
+<footer>
+
+
+</footer>
+
+<!-- SCRIPTS -->
+
+<!-- -->
+
+</body>
+</html>
